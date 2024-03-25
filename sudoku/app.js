28e3864234ca10b87a2bce6d578eb92335984a12
jasperBrumter <jasper.brumter@gmail.com>
2024-03-25 07:18:47 -0600

    20240325 - Merge prep

--- sudoku/app.js
+++ sudoku/app.js
@@ -144,6 +144,18 @@ ________________________________________________________________________________
 		[null,null,6,4,null,2,5,null,null],
 	];
 
+	const real_empty_entry = [
+		[null,null,null,null,null,null,null,null,null],
+		[null,null,null,null,null,null,null,null,null],
+		[null,null,null,null,null,null,null,null,null],
+		[null,null,null,null,null,null,null,null,null],
+		[null,null,null,null,null,null,null,null,null],
+		[null,null,null,null,null,null,null,null,null],
+		[null,null,null,null,null,null,null,null,null],
+		[null,null,null,null,null,null,null,null,null],
+		[null,null,null,null,null,null,null,null,null]
+	];
+
 	const empty_entry = [
 		[],
 		[],
@@ -156,7 +168,7 @@ ________________________________________________________________________________
 		[],
 	];
 
-	const USED_ENTRIES = cracking_entry;
+	const USED_ENTRIES = real_empty_entry;
 
 	const timer = 1;
 	const highlightTimer = timer;
@@ -178,9 +190,12 @@ ________________________________________________________________________________
 	const BLOCKS = [1,2,3];
 	const INTEGERS = [1,2,3,4,5,6,7,8,9];
 
+	let ALL_SQUARES = [];
+
+
 	const HIGHLIGHT_CLASS = 'highlight';
 	const SECONDARY_HIGHLIGHT_CLASS = 'secondary-highlight';
-
+	const BLINK_CLASS = 'blink-highlight';
 
 
 /*
@@ -278,7 +293,7 @@ ________________________________________________________________________________
 		} else if (rowIndex === 8) {
 			squareClass += ' border-bottom';
 		}
-		// Horizontal Lines
+		// Vertical Lines
 		if (columnIndex % 3 === 0) {
 			squareClass += ' border-left';
 		} else if (columnIndex === 8) {
@@ -338,7 +353,8 @@ ________________________________________________________________________________
 
 	const grid = document.getElementById('grid');
 	const subtitleDOM = document.getElementById('subtitle');
-
+	const solveButton = document.getElementById('solve');
+	const resetButton = document.getElementById('reset');
 
 /*
 ______________________________________________________________________________________________________
@@ -374,6 +390,26 @@ ________________________________________________________________________________
 			square.classList.remove(HIGHLIGHT_CLASS);
 			square.classList.remove(SECONDARY_HIGHLIGHT_CLASS);
 		}, highlightTimer);
+	}
+
+
+	const removeAnimateSquare = (squareId) => {
+		const square = document.getElementById(squareId);
+		square.classList.remove(BLINK_CLASS);
+	}
+	/*
+	 * Add temporary highlighted class
+	 *
+	 * @prop {String} subtitle    - the new subtitle
+	 *
+	 * @return {void}
+	 */
+	const animateSquare = (squareId) => {
+		const square = document.getElementById(squareId);
+		ALL_SQUARES.forEach(otherSquare => {
+			removeAnimateSquare(otherSquare);
+		})
+		square.classList.add(BLINK_CLASS);
 	}
 
 	/*
@@ -532,14 +568,38 @@ ________________________________________________________________________________
 	 */
 	const writePen = (rowIndex, columnIndex, number) => {
 		SUDOKU_TABLE[rowIndex][columnIndex] = {
-			pen: number,
+			pen: number === 0 ? null : number,
 			pencil: [],
 		}
-		document.getElementById(getSmallSquareDivId(rowIndex, columnIndex)).innerHTML = `<b>${number}</b>`;
-		removePencilledFromRelatedSquares(rowIndex, columnIndex, number);
-
+		if (number !== 0) {
+			document.getElementById(getSmallSquareDivId(rowIndex, columnIndex)).innerHTML = `<b>${number}</b>`;
+			removePencilledFromRelatedSquares(rowIndex, columnIndex, number);
+		}
 	};
 
+	/*
+	 * Remove pen number in specific square - only useful in case of bad input from user
+	 *
+	 * @prop {Number} rowIndex      - index of row
+	 * @prop {Number} columnIndex   - index of column
+	 * @prop {Number} number        - the number being penned
+	 *
+	 * @return {void}
+	 */
+	const removePen = (rowIndex, columnIndex, number) => {
+		SUDOKU_TABLE[rowIndex][columnIndex] = {
+			pen: null,
+			pencil: [],
+		}
+		const square = document.getElementById(getSmallSquareDivId(rowIndex, columnIndex));
+		square.innerHTML = '';
+		for (int_first_iteration of INTEGERS) {
+			const pencil = document.createElement('div');
+			pencil.className = `pencil pencil-${int_first_iteration}`;
+			pencil.id = getPencilDivId(rowIndex, columnIndex, int_first_iteration);
+			square.appendChild(pencil);
+		}
+	};
 /*
 ______________________________________________________________________________________________________
 ------------------------------------------------------------------------------------------------------
@@ -616,6 +676,11 @@ ________________________________________________________________________________
 ------------------------------------------------------------------------------------------------------
 */
 
+	const mobileAndTabletCheck = () => {
+	  let check = false;
+	  (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino|android|ipad|playbook|silk/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);
+	  return check;
+	};
 
 	/*
 	 * Modify DOM grid element with entries
@@ -623,6 +688,9 @@ ________________________________________________________________________________
 	 * @return {void}
 	 */
 	const initializeTable = () => {
+		if (USED_ENTRIES === real_empty_entry) {
+			grid.innerHTML = '';
+		}
 		// Create JS table
 		for (int_first_iteration of INTEGERS) {
 			// Add Data
@@ -648,27 +716,37 @@ ________________________________________________________________________________
 				SUDOKU_TABLE[rowIndex].push(square_info);
 
 				// Draw Table
-				const column = document.createElement('div');
-				column.id = getSmallSquareDivId(rowIndex, columnIndex);
-				column.className = getSmallSquareClass(rowIndex, columnIndex);;
-				row.appendChild(column);
+				const square = document.createElement('div');
+				square.id = getSmallSquareDivId(rowIndex, columnIndex);
+				square.className = getSmallSquareClass(rowIndex, columnIndex);;
+				row.appendChild(square);
 
 				for (int_third_iteration of INTEGERS) {
 					const pencil = document.createElement('div');
 					pencil.className = `pencil pencil-${int_third_iteration}`;
 					pencil.id = getPencilDivId(rowIndex, columnIndex, int_third_iteration);
-					column.appendChild(pencil);
+					square.appendChild(pencil);
 
 					// pencil.innerHTML = int_third_iteration;
 				}
 				// Add value and event listener
 				if (corresponding !== null) {
-					column.innerHTML =  `<b>${corresponding}<b>`;
+					square.innerHTML =  `<b>${corresponding}<b>`;
 				};
 				// TODO REMOVE
-				column.addEventListener('click', () => {
-					const squares = getRelatedSquares(rowIndex, columnIndex);
-					squares.forEach(square => highlightSquare(square));
+				square.addEventListener('click', () => {
+					const isMobile = mobileAndTabletCheck();
+					const squareId = getSmallSquareDivId(rowIndex, columnIndex);
+					animateSquare(squareId);
+					if (isMobile) {
+						const number = prompt("Please enter digit 1-9");
+						if (number >= 1 && number <= 9) {
+							writePen(rowIndex, columnIndex, number);
+						} else if (number == 0) {
+							removePen(rowIndex, columnIndex, number);
+						}
+		  				removeAnimateSquare(squareId);
+					}
 				})
 
 			};
@@ -958,7 +1036,7 @@ ________________________________________________________________________________
 ------------------------------------------------------------------------------------------------------
 */
 
-	const process = async () => {
+	const processSolve = async () => {
 		initializeTable();
 		await new Promise(resolve => setTimeout(resolve, timer * 2));
 		await traverseTreeAndFindPencilled();
@@ -1015,4 +1093,37 @@ ________________________________________________________________________________
 		await cleanUpPencilled()
 	}
 
-	process();
+	if (USED_ENTRIES === real_empty_entry) {
+		solveButton.addEventListener('click', () => {
+			solveButton.classList.add('hidden');
+			resetButton.classList.add('hidden');
+			processSolve();
+		})
+		resetButton.addEventListener('click', () => {
+			solveButton.classList.remove('hidden');
+			resetButton.classList.add('hidden');
+			processSetup();
+		})
+	}
+
+	const processSetup = async () => {
+		initializeTable();
+		const isMobile = mobileAndTabletCheck();
+		if (!isMobile) {
+			const fillTableListener = document.addEventListener('keydown', (event) => {
+			  	const blinking = document.getElementsByClassName(BLINK_CLASS);
+			  	if (event.keyCode >= 49 && event.keyCode <= 57 && blinking.length) {
+			  		const number = event.keyCode - 48;
+			  		const [rowId, columnId] = blinking[0].id.split('');
+			  		writePen(rowId, columnId, number);
+			  		removeAnimateSquare(blinking[0].id);
+			  	} else if (event.keyCode == 48 && blinking.length) {
+			  		const [rowId, columnId] = blinking[0].id.split('');
+			  		removePen(rowId, columnId, 0);
+			  		removeAnimateSquare(blinking[0].id);
+			  	}
+			});
+		}
+	};
+
+	processSetup();
